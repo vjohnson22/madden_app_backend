63dd1649657ca3574165c8cf76a846f3919c9332
Victor Johnson <vjohnson22@gmail.com>
2019-09-09 16:37:15 -0400

    updated models, starting to add rest frameowork

--- madden/models.py
+++ madden/models.py
@@ -33,6 +33,7 @@ class GameStat(models.Model):
     kick_return_yards = models.PositiveIntegerField()
     turnovers = models.PositiveIntegerField(default=0)
 
+    
 class Player(models.Model):
     name = models.CharField(max_length=100)
     photo_url = models.TextField()
@@ -65,6 +66,8 @@ class PlayerStat(models.Model):
     sacks = models.PositiveIntegerField(default=0)
     interceptions = models.PositiveIntegerField(default=0)
     defensive_tds = models.PositiveIntegerField(default=0)
+    def __str__(self):
+        return f'{self.name}: {self.game}'
 
 class SeasonStat(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='season_owner')
@@ -88,3 +91,5 @@ class SeasonStat(models.Model):
     forced_fumbles = models.PositiveIntegerField()
     interceptions = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f'{self.owner}: {self.season} Season'
--- madden_django/settings.py
+++ madden_django/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'madden'
+    'madden',
+    'django_extensions',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
