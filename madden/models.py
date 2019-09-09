1249578f750244cc235c381d59869faca41853c4
Victor Johnson <vjohnson22@gmail.com>
2019-09-09 15:34:13 -0400

    models updated to reflect 0 as default, and some other changes.

--- madden/models.py
+++ madden/models.py
@@ -5,13 +5,20 @@ from django.db import models
 
 class Owner(models.Model):
     name = models.CharField(max_length=100)
-    photo_url = models.TextField()
+    photo_url = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
+
 
 class Game(models.Model):
     season = models.PositiveIntegerField()
     week = models.PositiveIntegerField()
     won = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='wins')
     lost = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='losses')
+
+    def __str__(self):
+        return f'{self.won} vs {self.lost}: Week {self.week}, {self.season}'
 
 class GameStat(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='game_stats')
@@ -24,35 +31,40 @@ class GameStat(models.Model):
     first_downs = models.PositiveIntegerField()
     punt_return_yards = models.PositiveIntegerField()
     kick_return_yards = models.PositiveIntegerField()
+    turnovers = models.PositiveIntegerField(default=0)
 
 class Player(models.Model):
     name = models.CharField(max_length=100)
     photo_url = models.TextField()
+    position = models.CharField(max_length=2, default=0)
+
+    def __str__(self):
+        return self.name
+
 
 class PlayerStat(models.Model):
     name = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player')
-    position = models.CharField(max_length= 2)
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='game')
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='player_owner')
     against = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='owner_played_against')
-    pass_yards = models.PositiveIntegerField(blank=True)
-    pass_td = models.PositiveIntegerField(blank=True)
-    pass_int = models.PositiveIntegerField(blank=True)
-    times_sacked = models.PositiveIntegerField(blank=True)
-    pass_complete = models.PositiveIntegerField(blank=True)
-    pass_attempt  = models.PositiveIntegerField(blank=True)
-    rush_yards = models.PositiveIntegerField(blank=True)
-    rush_tds = models.PositiveIntegerField(blank=True)
-    fumbled = models.PositiveIntegerField(blank=True)
-    break_tackle = models.PositiveIntegerField(blank=True) 
-    receptions = models.PositiveIntegerField(blank=True)
-    receiving_yards =models.PositiveIntegerField( blank=True)
-    receiving_tds = models.PositiveIntegerField( blank=True)
-    tackles = models.PositiveIntegerField(blank=True)
-    tfl = models.PositiveIntegerField(blank=True)
-    sacks = models.PositiveIntegerField(blank=True)
-    interceptions = models.PositiveIntegerField(blank=True)
-    defensive_tds = models.PositiveIntegerField(blank=True)
+    pass_yards = models.PositiveIntegerField(default=0)
+    pass_td = models.PositiveIntegerField(default=0)
+    pass_int = models.PositiveIntegerField(default=0)
+    times_sacked = models.PositiveIntegerField(default=0)
+    pass_complete = models.PositiveIntegerField(default=0)
+    pass_attempt  = models.PositiveIntegerField(default=0)
+    rush_yards = models.PositiveIntegerField(default=0)
+    rush_tds = models.PositiveIntegerField(default=0)
+    fumbled = models.PositiveIntegerField(default=0)
+    break_tackle = models.PositiveIntegerField(default=0) 
+    receptions = models.PositiveIntegerField(default=0)
+    receiving_yards =models.PositiveIntegerField( default=0)
+    receiving_tds = models.PositiveIntegerField( default=0)
+    tackles = models.PositiveIntegerField(default=0)
+    tfl = models.PositiveIntegerField(default=0)
+    sacks = models.PositiveIntegerField(default=0)
+    interceptions = models.PositiveIntegerField(default=0)
+    defensive_tds = models.PositiveIntegerField(default=0)
 
 class SeasonStat(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='season_owner')
