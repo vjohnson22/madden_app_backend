a3cafc88c13eb4d996a1ab8e1caf1d6775671d17
Victor Johnson <vjohnson22@gmail.com>
2019-09-09 11:23:14 -0400

    working on models

--- madden/models.py
+++ madden/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Owner(models.Model):
+    name = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+class Game(models.Model):
+    season = models.CharField(max_length=4)
+    week = models.CharField(max_length=2)
+    won = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='wins')
+    lost = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='losses')
