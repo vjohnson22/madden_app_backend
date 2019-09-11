2a026b6ee9ed0d72c9f4fd3397eead74d4ae5226
Victor Johnson <vjohnson22@gmail.com>
2019-09-11 14:18:16 -0400

    updated settings

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: gunicorn madden_django.wsgi --log-file -
 
-web: python manage.py runserver 0.0.0.0:$PORT
--- madden_django/settings.py
+++ madden_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url 
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +25,7 @@ SECRET_KEY = 'cw8-+yg)ts!6v6fy&in9$wjf4z5dg==0eb6wm_e+fi!h_0%mg2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['maddenstatsapp.herokuapp.com']
+ALLOWED_HOSTS = ['maddenstatsapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -39,9 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'madden',
-    'django_extensions',
     'rest_framework',
-    # 'corsheaders'
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
@@ -52,9 +51,9 @@ REST_FRAMEWORK = {
     ]
 }
 MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -93,7 +92,7 @@ DATABASES = {
         'NAME': 'madden',
         'USER': 'maddenuser',
         'PASSWORD': 'madden',
-        'HOST': 'localhost'
+        # 'HOST': 'localhost'
     }
 }
 
@@ -131,25 +130,25 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/1.11/howto/static-files/
+# PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+# STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 
 
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
