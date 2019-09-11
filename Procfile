ef2d61e4c579c7f89df0e9424bcb0ae0c9b0d0cd
Victor Johnson <vjohnson22@gmail.com>
2019-09-11 10:15:43 -0400

    add 0.0.0.0

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn madden_django.wsgi --log-file -
+
+web: python manage.py runserver 0.0.0.0:$PORT
--- madden_django/settings.py
+++ madden_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,7 +148,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
-import dj_database_url 
+
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
